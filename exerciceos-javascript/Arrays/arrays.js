cf63fe56781a2ea19b93c2ffd45a1d0bcfab2ccc
chrystofferhandriell <chrystofferhandriell5@gmail.com>
2026-01-15 20:09:08 -0300

    adicionando novas aulas e explos

---
+++ exerciceos-javascript/Arrays/arrays.js
@@ -0,0 +1,6 @@
+
+let frutas = ['Maçã','Banana', 'Manga'];
+ console.log(frutas[0]); /* Saída: ‘Maçã’*/
+
+ frutas.push('Uva'); /* Adiciona ao final */
+frutas.pop(); /* Remove o último elemento*/
---
+++ exerciceos-javascript/Objetos/Objetos.js
@@ -0,0 +1,9 @@
+let pessoa = {
+     nome: 'João',
+     idade: 30,
+     profissao: 'Engenheiro' }; 
+
+console.log(pessoa.idade); /* Saída: ‘João’*/
+
+pessoa.nacionalidade = 'Brasileiro'; /*Adicionando uma propriedade */
+pessoa.idade = 31; /*Modificando uma propriedade existente*/
