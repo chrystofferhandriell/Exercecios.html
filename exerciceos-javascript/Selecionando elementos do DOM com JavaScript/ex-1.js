8ea7fb4f4254e275431577a0cc918d93d16a2572
chrystofferhandriell <chrystofferhandriell5@gmail.com>
2026-02-24 15:17:58 -0300

    novas aulas de javascript com implementaçao com html

---
+++ exerciceos-javascript/Selecionando elementos do DOM com JavaScript/ex-1.js
@@ -0,0 +1,8 @@
+const test = document.querySelector('#myDiv');
+console.log(test)
+
+const ex2 = document.querySelectorAll('.highlight')
+console.log(ex2);
+
+const ex3 = document.getElementsByClassName("oldStyle");
+console.log(ex3);
